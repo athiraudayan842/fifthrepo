d8c15cbd824564fff2000460e5cda409d09d49f3
Admin <Admin@DESKTOP-5IN5P6J>
2025-01-07 11:01:33 +0530

    assignment2

---
+++ Assignment2/src/model1/Sample8.java
@@ -0,0 +1,17 @@
+package model1;
+
+public class Sample8 {
+
+	public static void main(String[] args) {
+	Sample8 obj=new Sample8();
+	obj.sample1();
+	}
+	public void sample1()
+	{
+		int a=3,b=4,sum;
+		sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
